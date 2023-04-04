b4c167521021a60e1fc5942acfb03fee962b5fdc
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-04 19:46:36 +0600

    error component added

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root> </Root>
+    element: <Root> </Root>,
+    errorElement: <ErrorPage />,
   },
 ]);
 
