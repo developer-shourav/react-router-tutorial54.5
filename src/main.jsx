aac559a1f8e5d8ed191c027b9a77935c407687e0
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-04 21:44:51 +0600

    form content added

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Contact, {
   loader as contactLoader,
 } from "./routes/contact";
 import Root, { loader as rootLoader ,  action as rootAction,} from "./routes/root";
+import EditContact, { action as editAction, } from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,12 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,5 +1,13 @@
-import { Form, useLoaderData } from "react-router-dom";
+import { Form, useLoaderData, redirect, } from "react-router-dom";
+import { updateContact } from "../contacts";
 
+export async function action({ request, params }) {
+    const formData = await request.formData();
+    const updates = Object.fromEntries(formData);
+    await updateContact(params.contactId, updates);
+    return redirect(`/contacts/${params.contactId}`);
+  }
+  
 export default function EditContact() {
   const { contact } = useLoaderData();
 
