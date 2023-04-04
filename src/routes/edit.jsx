e3b5db148fed3d9d7579725af7513bb374304b64
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-04 22:18:05 +0600

    active nav link added

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -3,11 +3,15 @@ import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
     const formData = await request.formData();
+    const firstName = formData.get("first");
+    const lastName = formData.get("last");
     const updates = Object.fromEntries(formData);
+    updates.first; // "Some"
+    updates.last; // "Name"
     await updateContact(params.contactId, updates);
     return redirect(`/contacts/${params.contactId}`);
   }
-  
+
 export default function EditContact() {
   const { contact } = useLoaderData();
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,9 @@
-import { Link, Outlet, useLoaderData, Form,} from "react-router-dom";
+import { Link, Outlet, NavLink, useLoaderData, Form, redirect,} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
@@ -40,7 +40,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -49,7 +58,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -71,3 +80,5 @@ export default function Root() {
     const contacts = await getContacts();
     return { contacts };
   }
+
+  
